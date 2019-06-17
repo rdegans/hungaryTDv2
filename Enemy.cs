b630b1ae3aaa5be64c711b70ff1e180e694ef607
rdegans <38084642+rdegans@users.noreply.github.com>
2019-06-17 11:12:57 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace hungaryTDv2
+{
+    public class Enemy
+    {
+        public ImageBrush enemyFill = new ImageBrush();
+        public BitmapImage bi;
+        public Type type;
+        public enum Type { apple, pizza, donut, hamburger, fries }
+        public Canvas cEnemies = new Canvas();
+        public Canvas cBackground = new Canvas();
+        public Rectangle sprite = new Rectangle();
+        public int speed;
+        public int health;
+        public int damage;
+        public Point[] track;
+        public int[] positions;
+        public int reward;
+        public int position = 0;
+        public Enemy(int ty, Canvas cE, Canvas cB, Point[] tr, int[] p)
+        {
+            type = (Type)ty;
+            cEnemies = cE;
+            cBackground = cB;
+            track = tr;
+            positions = p;
+            if (type == Type.apple)
+            {
+                bi = new BitmapImage(new Uri("apple.png", UriKind.Relative));
+                speed = 3;
+                health = 3;
+                damage = 50;
+                reward = 100;
+            }
+            else if (type == Type.pizza)
+            {
+                bi = new BitmapImage(new Uri("pizza.png", UriKind.Relative));
+                speed = 5;
+                health = 200;
+                damage = 50;
+                reward = 100;
+            }
+            else if (type == Type.donut)
+            {
+                bi = new BitmapImage(new Uri("donut.png", UriKind.Relative));
+                speed = 3;
+                health = 3;
+                damage = 100;
+                reward = 100;
+            }
+            else if (type == Type.hamburger)
+            {
+                bi = new BitmapImage(new Uri("hamburger.png", UriKind.Relative));
+                speed = 2;
+                health = 50;
+                damage = 500;
+                reward = 100;
+            }
+            else if (type == Type.fries)
+            {
+                bi = new BitmapImage(new Uri("fries.png", UriKind.Relative));
+                speed = 10;
+                health = 5;
+                damage = 50;
+                reward = 100;
+            }
+            enemyFill = new ImageBrush(bi);
+            sprite.Fill = enemyFill;
+            sprite.Height = 50;
+            sprite.Width = 50;
+            Canvas.SetLeft(sprite, track[position].X - 25);
+            Canvas.SetTop(sprite, track[position].Y - 25);
+            cEnemies.Children.Add(sprite);
+            cBackground.Children.Remove(cEnemies);
+            cBackground.Children.Add(cEnemies);
+        }
+        public int update(int index)
+        {
+            positions[position] = -1;
+            for (int i = 1; i < 10; i++)
+            {
+                if (position + i < positions.Length)
+                {
+                    positions[position + i] = -1;
+                }
+                if (position - i > -1)
+                {
+                    positions[position - i] = -1;
+                }
+            }
+            if (position < 1450 - speed - 9)
+            {
+                for (int i = 0; i < speed + 1; i++)
+                {
+                    if (positions[position + i + 9] != -1)
+                    {
+                        position = position + i - 1;
+                        positions[position] = index;
+                        for (int x = 1; x < 10; x++)
+                        {
+                            if (position + x < positions.Length)
+                            {
+                                positions[position + x] = index;
+                            }
+                            if (position - x > -1)
+                            {
+                                positions[position - x] = index;
+                            }
+                        }
+                        break;
+                    }
+                    else if (i == speed && positions[position + i] == -1)
+                    {
+                        position = position + i - 1;
+                        positions[position] = index;
+                        for (int x = 1; x < 10; x++)
+                        {
+                            if (position + x < positions.Length)
+                            {
+                                positions[position + x] = index;
+                            }
+                            if (position - x > -1)
+                            {
+                                positions[position - x] = index;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+                Canvas.SetLeft(sprite, track[position].X - 25);
+                Canvas.SetTop(sprite, track[position].Y - 25);
+                return 0;
+            }
+            else
+            {
+                cEnemies.Children.Remove(sprite);
+                return damage;
+            }
+        }
+    }
+}
+/*                        try
+            {
+                sw = new StreamWriter("line1.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                sw = new StreamWriter("line1.txt");
+            }
+            for (int i = 0; i < 175; i+=1)
+            {
+                sw.WriteLine(i + ",275");
+            }
+            for (int i = 275; i > 205; i-=1)
+            {
+                sw.WriteLine("175," + i);
+            }
+            for (int i = 175; i < 315; i += 1)
+            {
+                sw.WriteLine(i + ",205");
+            }
+            for (int i = 205; i < 490; i += 1)
+            {
+                sw.WriteLine("315," + i);
+            }
+            for (int i = 315; i < 525; i += 1)
+            {
+                sw.WriteLine(i + ",490");
+            }
+            for (int i = 490; i > 275; i-=1)
+            {
+                sw.WriteLine("525," + i);
+            }
+            for (int i = 525; i < 675; i += 1)
+            {
+                sw.WriteLine(i + ",275");
+            }
+            for (int i = 275; i < 345; i++)
+            {
+                sw.WriteLine("675," + i);
+            }
+            for (int i = 675; i < 810; i += 1)
+            {
+                sw.WriteLine(i + ",345");
+            }
+            sw.Close();
+*/
+
+
+
+
+/*            /*
+             * track:
+0,240
+140,240
+140,170
+350,170
+350,455
+490,455
+490,240
+700,240
+700,310
+750,310
+750,210
+845,210
+845,380
+820,410
+730,380
+630,380
+630,310
+560,310
+560,525
+280,525
+280,240
+210,240
+210,310
+0,310
+           * menu:
+0,120
+845,120
+845,650
+1125,650
+1125,0
+0,0
+0,120
+            * polyline
+0,275
+175,275
+175,205
+315,205
+315,490
+525,490
+525,275
+675,275
+675,345
+810,345
+        * fork
+2.666666666667,19.333333333333
+6.666666666667,16.666666666667
+6.666666666667,6
+10,3.333333333333
+10,-0.6666666666666
+0.666666666667,0
+-0.6666666666666,4.666666666667
+2.666666666667,6.666666666667
+             */
+/*Welcome to the first level! A wave of angry food is coming, spend your money to protect the sacred fridge!
+0,0,0,0,0,0,0,0,0,0
+That wasn't that bad, was it? Well these next enemies ARE that bad, prepare yourself!
+0,0,0,0,1,1,1,1,1
+You're stronger than I thought. Let's see how you handle this next wave
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+I bet you're wondering what the police tower does. Maybe you should give them a try.
+0,0,0,0,1,1,1,1,2,2
+Hopefully you figured it out, donuts can only be eaten by police.Make sure you're ready for them.
+2,2,2,2,1,1,1,1,1,1,1,1,1,1
+It looked like you almost had a hard time with that last wave.Are you sure you're good enough to do this?
+1,1,1,1,1,2,2,2,2,2,0,0,0,0,0,3
+hAmBuRgEr!?...
+1,1,1,2,2,2,4,4,4,4
+Those fries are pretty speedy, eh? Make sure you have a fast firing tower, so you're sure to hit them.
+3,3,4,4,4,4,4,4
+Here's a real challenge, this wave is massive!(Your computer might not have enough ram for this)
+4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+Fast didn't work, maybe slow and steady protects the fridge.
+3,3,3,3,3,3,3,3,3,3*/
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace hungaryTDv2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Rectangle background;
+        public Label lblMouseTest;
+        public Button tempTwrBtn;
+        public Rectangle tempRect;
+        public Rectangle healthBar = new Rectangle();
+        public Rectangle damageBar = new Rectangle();
+        public Label lblHealth = new Label();
+        public Label lblMoney = new Label();
+        public bool mouseTest;
+        public Button[] towerIcons = new Button[4];
+        public Button btnStart = new Button();
+        public ImageBrush[] towerFill = new ImageBrush[4];
+        public System.Windows.Threading.DispatcherTimer gameTimer = new System.Windows.Threading.DispatcherTimer();
+        public GameState gameState;
+        public enum GameState { play, store, test };
+        public TowerType towerType;
+        public enum TowerType { normal, police, family, tank }
+        public EnemyType enemyType;
+        public enum EnemyType { apple, pizza, donut, hamburger, fries }
+        public List<Enemy> enemies = new List<Enemy>();
+        public Polygon trackHit = new Polygon();
+        public Point[] track = new Point[1450];
+        public int[] positions = new int[1450];
+        public StreamWriter sw;
+        public StreamReader sr;
+        public int tempTowerType;
+        public int tempCost;
+        public int money = 300;
+        public List<Tower> towers = new List<Tower>();
+        public int level = 0;
+        public int[][] waves = new int[10][];
+        public string[] levelMessages = new string[10];
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnStart.Height = 20;
+            btnStart.Width = 70;
+            btnStart.Content = "start";
+            btnStart.Click += BtnStart_Click;
+            cBackground.Children.Add(btnStart);
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = -1;
+            }
+            sr = new StreamReader("trackLine.txt");
+            int counter = 0;
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+                double xPosition, yPosition;
+                double.TryParse(currentLine.Split(',')[0], out xPosition);
+                double.TryParse(currentLine.Split(',')[1], out yPosition);
+                Point point = new Point(xPosition, yPosition);
+                track[counter] = point;
+                counter++;
+            }
+            sr.Close();
+            gameState = GameState.play;
+            counter = 0;
+            sr = new StreamReader("levels.txt");
+            while (!sr.EndOfStream)
+            {
+                string currentLine;
+                currentLine = sr.ReadLine();
+                try
+                {
+                    string[] lineSplit = currentLine.Split();
+                    for(int i = 0; i < lineSplit.Length; i++)
+                    {
+                        int.TryParse(lineSplit[i], out waves[counter/2][i]);
+                    }
+                }
+                catch
+                {
+                    levelMessages[counter / 2] = currentLine;
+                }
+                counter++;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            if (gameState == GameState.store)
+            {
+                Canvas.SetTop(tempRect, Mouse.GetPosition(cBackground).Y - tempRect.Height / 2);
+                Canvas.SetLeft(tempRect, Mouse.GetPosition(cBackground).X - tempRect.Width / 2);
+                bool valid = true;
+                double x = Mouse.GetPosition(cBackground).X;
+                double y = Mouse.GetPosition(cBackground).Y;
+                bool check1 = cObstacles.InputHitTest(new Point(x + tempRect.Width / 2 - 5, y + tempRect.Height / 2 - 5)) == null;
+                bool check2 = cObstacles.InputHitTest(new Point(x - tempRect.Width / 2 + 5, y + tempRect.Height / 2 - 5)) == null;
+                bool check3 = cObstacles.InputHitTest(new Point(x + tempRect.Width / 2 - 5, y - tempRect.Height / 2 + 5)) == null;
+                bool check4 = cObstacles.InputHitTest(new Point(x - tempRect.Width / 2 + 5, y - tempRect.Height / 2 + 5)) == null;
+                bool check5 = cObstacles.InputHitTest(new Point(x, y)) == null;
+                bool check6 = tempCost <= money;
+                if (check1 && check2 && check3 && check4 && check5 && check6)
+                {
+                    valid = true;
+                    tempRect.Stroke = Brushes.Transparent;
+                }
+                else
+                {
+                    valid = false;
+                    tempRect.Stroke = Brushes.Red;
+                    tempRect.StrokeThickness = 5;
+                }
+                MouseButtonState pmbs = MouseButtonState.Released;
+                if (Mouse.LeftButton == MouseButtonState.Pressed)
+                {
+                    if (valid)
+                    {
+                        Point temp = Mouse.GetPosition(cBackground);
+                        cBackground.Children.Remove(tempRect);
+                        Tower newTower = new Tower(tempTowerType, cBackground, cObstacles, positions, track, temp, cEnemies);
+                        towers.Add(newTower);
+                        money -= tempCost;
+                        lblMoney.Content = "$ " + money;
+                    }
+                    else
+                    {
+                        cBackground.Children.Remove(tempRect);
+                    }
+                    cObstacles.Children.Remove(trackHit);
+                    gameState = GameState.play;
+                }
+                else
+                {
+                    pmbs = Mouse.LeftButton;
+                }
+            }
+            else if (gameState == GameState.test)
+            {
+            }
+            else if (gameState == GameState.play)
+            {
+                if (enemies.Count == 0)
+                {
+                }
+                for (int i = enemies.Count - 1; i > -1; i--)
+                {
+                    int tempDamage = enemies[i].update(i);
+                    if (tempDamage > 0)
+                    {
+                        damageBar.Width += tempDamage;
+                        Canvas.SetLeft(damageBar, 825 - damageBar.Width);
+                        enemies.RemoveAt(i);
+                    }
+                }
+                for (int i = 0; i < towers.Count; i++)
+                {
+                    List<Shape> hits = towers[i].Shoot();
+                    if (hits != null && hits.Count > 0)
+                    {
+                        for (int x = 0; x < hits.Count; x++)
+                        {
+                            for (int y = 0; y < enemies.Count; y++)
+                            {
+                                if (enemies[y].sprite == hits[x])
+                                {
+                                    enemies[y].health -= towers[i].damage;
+                                    if (enemies[y].health <= 0)
+                                    {
+                                        money += enemies[y].reward;
+                                        lblMoney.Content = "$ " + money;
+                                        cEnemies.Children.Remove(enemies[y].sprite);
+                                        for (int a = 0; a < positions.Length; a++)
+                                        {
+                                            if (positions[a] == y)
+                                            {
+                                                positions[a] = -1;
+                                            }
+                                        }
+                                        enemies.RemoveAt(y);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            gameTimer.Start();
+
+            sr = new StreamReader("trackBox.txt");
+            PointCollection myPointCollection = new PointCollection();
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+                double xPosition, yPosition;
+                double.TryParse(currentLine.Split(',')[0], out xPosition);
+                double.TryParse(currentLine.Split(',')[1], out yPosition);
+                Point point = new Point(xPosition, yPosition);
+                myPointCollection.Add(point);
+            }
+            sr.Close();
+            trackHit.Points = myPointCollection;
+            trackHit.Fill = Brushes.Transparent;
+
+            cBackground.Children.Remove(btnStart);
+            background = new Rectangle();
+            background.Height = 650;
+            background.Width = 1125;
+            BitmapImage bi = new BitmapImage(new Uri("track.png", UriKind.Relative));
+            ImageBrush img = new ImageBrush(bi);
+            background.Fill = img;
+            cBackground.Children.Add(background);
+
+            tempTwrBtn = new Button();
+            tempTwrBtn.Height = 20;
+            tempTwrBtn.Width = 40;
+            tempTwrBtn.Content = "test";
+            tempTwrBtn.Click += TempTwrBtn_Click;
+            Canvas.SetTop(tempTwrBtn, 17);
+            Canvas.SetLeft(tempTwrBtn, 1000);
+            cBackground.Children.Add(tempTwrBtn);
+
+            bi = new BitmapImage(new Uri("normal.png", UriKind.Relative));
+            towerFill[0] = new ImageBrush(bi);
+            bi = new BitmapImage(new Uri("police.png", UriKind.Relative));
+            towerFill[1] = new ImageBrush(bi);
+            bi = new BitmapImage(new Uri("family.png", UriKind.Relative));
+            towerFill[2] = new ImageBrush(bi);
+            bi = new BitmapImage(new Uri("tank.png", UriKind.Relative));
+            towerFill[3] = new ImageBrush(bi);
+            for (int i = 0; i < towerIcons.Length; i++)
+            {
+                towerIcons[i] = new Button();
+                towerIcons[i].Background = towerFill[i];
+                towerIcons[i].Height = 80;
+                towerIcons[i].Width = 80;
+                towerIcons[i].Click += iconsClick;
+                towerIcons[i].BorderBrush = Brushes.Transparent;
+                Canvas.SetTop(towerIcons[i], i * 150 + 60);
+                Canvas.SetLeft(towerIcons[i], 910);
+                cBackground.Children.Add(towerIcons[i]);
+            }
+            gameState = GameState.play;
+
+            healthBar.Height = 25;
+            healthBar.Width = 250;
+            healthBar.Fill = Brushes.Green;
+            healthBar.Stroke = Brushes.Black;
+            Canvas.SetTop(healthBar, 85);
+            Canvas.SetLeft(healthBar, 575);
+            cBackground.Children.Add(healthBar);
+
+            damageBar.Height = 25;
+            damageBar.Width = 0;
+            damageBar.Fill = Brushes.DarkRed;
+            damageBar.Stroke = Brushes.Black;
+            Canvas.SetTop(damageBar, 85);
+            Canvas.SetLeft(damageBar, 575);
+            cBackground.Children.Add(damageBar);
+
+            lblHealth.Foreground = Brushes.Black;
+            lblHealth.Content = "Health";
+            lblHealth.FontWeight = FontWeights.UltraBold;
+            lblHealth.FontFamily = new FontFamily("Consola");
+            lblHealth.FontSize = 20;
+            lblHealth.Height = 50;
+            lblHealth.Width = 250;
+            Canvas.SetTop(lblHealth, 80);
+            Canvas.SetLeft(lblHealth, 575);
+            cBackground.Children.Add(lblHealth);
+
+            lblMoney.Foreground = Brushes.Gold;
+            lblMoney.Content = "Health";
+            lblMoney.FontWeight = FontWeights.UltraBold;
+            lblMoney.FontFamily = new FontFamily("Consola");
+            lblMoney.FontSize = 30;
+            lblMoney.Height = 50;
+            lblMoney.Width = 100;
+            Canvas.SetTop(lblMoney, 10);
+            Canvas.SetLeft(lblMoney, 875);
+            lblMoney.Content = "$ " + money;
+            cBackground.Children.Add(lblMoney);
+
+            enemies.Add(new Enemy((int)EnemyType.hamburger, cEnemies, cBackground, track, positions));
+            enemies.Add(new Enemy((int)EnemyType.apple, cEnemies, cBackground, track, positions));
+            enemies.Add(new Enemy((int)EnemyType.pizza, cEnemies, cBackground, track, positions));
+            enemies.Add(new Enemy((int)EnemyType.pizza, cEnemies, cBackground, track, positions));
+            enemies.Add(new Enemy((int)EnemyType.pizza, cEnemies, cBackground, track, positions));
+        }
+
+        private void TempTwrBtn_Click(object sender, RoutedEventArgs e)
+        {
+            gameState = GameState.test;
+        }
+        private void iconsClick(object sender, RoutedEventArgs e)
+        {
+            //sw.Close();
+            gameState = GameState.store;
+            cObstacles.Children.Add(trackHit);
+            Button button = sender as Button;
+            for (int i = 0; i < towerIcons.Length; i++)
+            {
+                if (towerIcons[i] == button)
+                {
+                    tempTowerType = i;
+                }
+            }
+            tempRect = new Rectangle();
+            tempRect.Fill = towerFill[tempTowerType];
+            if (tempTowerType < 2)
+            {
+                tempRect.Height = 35;
+                tempRect.Width = 35;
+                tempCost = 100;
+            }
+            else if (tempTowerType == 2)
+            {
+                tempRect.Height = 45;
+                tempRect.Width = 70;
+                tempCost = 100;
+            }
+            else
+            {
+                tempRect.Height = 70;
+                tempRect.Width = 70;
+                tempCost = 100;
+            }
+            cBackground.Children.Add(tempRect);
+        }
+    }
+}
